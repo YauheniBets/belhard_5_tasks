a9ac040475e6818b759ab26b36850fae18ce02b4
Evgeniy Bets <n3shta2007@yandex.ru>
2021-09-13 10:40:52 +0300

    can calculate seconds now

--- tasks/easy/if_elif_else/clock.py
+++ tasks/easy/if_elif_else/clock.py
@@ -44,7 +44,13 @@ def get_seconds(h: int, m: int, s: int) -> Union[int, str]:
     :return: количество сеекунд от 00:00:00
     :rtype: int
     """
-    result = None
+    result = (h * 3600) + (m * 60) + s
+    if  h > 23 or h < 0:
+        result = "Ошибка. Допустимое значение для часов 0..23"
+    elif m > 59 or m < 0:
+        result = 'Ошибка. Допустимое значение для минут 0..59'
+    elif s > 59 or s < 0:
+        result = 'Ошибка. Допустимое значение для секунд 0..59'
     return result
 
 
